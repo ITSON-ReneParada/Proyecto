75298029644cf3edfbe77273d9cb212e762be1df
Jesus Grajeda <58005776+lisoas@users.noreply.github.com>
2022-04-25 15:50:20 -0700

    Avance 4

--- routes/supermercadoRoutes.js
+++ routes/supermercadoRoutes.js
@@ -2,13 +2,12 @@ const express = require('express');
 const supermercadoRouter = express.Router();
 const { createSupermercado, deleteSupermercado, editSupermercado, getAllSupermercados, getSupermercado } = require('../controllers/supermercadoController.js')
 
-supermercadoRouter
-.route('/')
-.get(getAllSupermercados)
-.get(getSupermercado)
-.post(createSupermercado)
-.put(editSupermercado)
-.delete(deleteSupermercado)
+
+supermercadoRouter.get(getAllSupermercados)
+supermercadoRouter.get(getSupermercado)
+supermercadoRouter.post(createSupermercado)
+supermercadoRouter.put(editSupermercado)
+supermercadoRouter.delete(deleteSupermercado)
 
 
 module.exports = supermercadoRouter;
